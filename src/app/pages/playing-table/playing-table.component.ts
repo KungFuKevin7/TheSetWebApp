2838178045bb95d1c9a21c12883893fa278b7338
KungFuKevin <kevin.voet@student.nhlstenden.com>
2024-11-13 13:52:16 +0100

    added new images

--- src/app/pages/playing-table/playing-table.component.ts
+++ src/app/pages/playing-table/playing-table.component.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
 import {GameHistoryComponent} from "../game-history/game-history.component";
 import {GamePageComponent} from '../game-page/game-page.component';
+import {PlayingCardComponent} from '../playing-card/playing-card.component';
 
 @Component({
   selector: 'app-playing-table',
   standalone: true,
   imports: [
     GameHistoryComponent,
-    GamePageComponent
+    GamePageComponent,
+    PlayingCardComponent
   ],
   templateUrl: './playing-table.component.html',
   styleUrl: './playing-table.component.css'
 })
 export class PlayingTableComponent {
+  playingCards : any = [1,2,3,4,5,6,7,8,9,10,11,12];
 
 }
